fd940d100b344db1145fbb1a04dee01b5483cdaa
Ray <songraysmail@gmail.com>
2024-09-22 11:39:08 +0800

    feat: upload

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,30 @@
 import { Hono } from 'hono'
 
-const app = new Hono()
+const app = new Hono().basePath('/api')
 
-app.get('/', (c) => {
-  return c.text('Hello, World!')
+app.post('/upload', async (c) => {
+  const env = c.env as { TOKEN: string; BUCKET: R2Bucket }
+  const token = c.req.header('Authorization')
+
+  if (token !== env.TOKEN) {
+    return c.text('Unauthorized', 401)
+  }
+
+  const body = await c.req.parseBody()
+  const file = body['file']
+
+  if (!file) {
+    return c.text('File not provided', 400)
+  }
+
+  if (typeof file === 'string') {
+    return c.text('Invalid type', 400)
+  }
+
+  const fileStream = file.stream()
+  await env.BUCKET.put(file.name, fileStream)
+
+  return c.text('File uploaded successfully')
 })
 
 export default app
